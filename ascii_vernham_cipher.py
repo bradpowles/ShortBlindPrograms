e28098c4f0daef986999e947af0a777f8d540b66
Tom Bell <tomedwardbell@gmail.com>
2020-03-05 15:09:43 +0000

    Update ascii_vernham_cipher.py

--- ascii_vernham_cipher.py
+++ ascii_vernham_cipher.py
@@ -1,2 +1,2 @@
-def encrypt2(message, keyword):
-    return("".join([chr(((ord(message[i]) - 65) + (ord(keyword[i % len(keyword)]) - 64)) % 26 + 65) for i in range(len(message))]))
+def ascii_vernham_cipher(input_text, keyword):
+    return("".join([chr(((ord(input_text[i]) - 65) + (ord(keyword[i % len(keyword)]) - 64)) % 26 + 65) for i in range(len(input_text))]))
